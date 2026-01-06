6e4505dd55c84333e6512d4307f0bedad034ade3
Random Engineers <RandomEngineers23802@gmail.com>
2026-01-05 21:10:56 -0600

    Removed unused auto aim. Final Kevin code for now. Will now clone respitory to begin turret bot code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/customClasses/robotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/customClasses/robotControl.java
@@ -5,11 +5,9 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.pedropathing.follower.Follower;
-import com.pedropathing.geometry.Pose;
 
 public class robotControl {
 
@@ -108,38 +106,5 @@ public class robotControl {
 
     public void blueBoiClosed() {
         BlueBoi.setPosition(0.65);
-    }
-
-    public void autoAim() {
-        LLResult result = limelight.getLatestResult();
-
-        if (result != null && result.isValid()) {
-            double tx = result.getTx();
-            double currentHeading = follower.getPose().getHeading();
-
-            double targetHeading = currentHeading + Math.toRadians(tx);
-            follower.turnTo(targetHeading);
-        }
-        else {
-            Pose currentPose = follower.getPose();
-            double errorX = targetGoalX - currentPose.getX();
-            double errorY = targetGoalY - currentPose.getY();
-
-            double targetAngle = Math.atan2(errorY, errorX) + Math.PI;
-
-            double angularError = targetAngle - currentPose.getHeading();
-            while (angularError > Math.PI) angularError -= 2 * Math.PI;
-            while (angularError < -Math.PI) angularError += 2 * Math.PI;
-
-            if (Math.abs(angularError) > Math.toRadians(10.0)) {
-                double setPower = 0.5;
-
-                double rotationPower = Math.signum(angularError) * setPower;
-
-                follower.setTeleOpDrive(0, 0, rotationPower, false);
-            } else {
-                follower.setTeleOpDrive(0, 0, 0, false);
-            }
-        }
     }
 }
