0cf9b7010a2cbe92f4b439962ea99637a4cc9f00
Random Engineers <RandomEngineers23802@gmail.com>
2026-03-10 17:45:41 -0500

    Modified robotControl and added alliance enum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Alliance.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.control;
+
+public enum Alliance {
+    BLUE,
+    RED
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/robotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/robotControl.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode.control;
 
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.PedroCoordinates;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -12,31 +19,35 @@ import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.util.Range;
 import com.pedropathing.math.Vector;
 
+import org.firstinspires.ftc.robotcore.external.function.Supplier;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
 public class robotControl {
-//May need differnetn aiming depending on if auto was close or far starting position
     public DcMotorEx ShooterL;
     public DcMotorEx ShooterR;
     public Limelight3A limelight;
     private DcMotor intake;
-    private DcMotor belt ;
+    public DcMotorEx belt;
     private Servo BlueBoi;
     public Servo kickstand1;
     public Servo kickstand2;
     private Follower follower;
     private Pose goalTarget;
+    public Supplier<PathChain> endgamePark;
     private ElapsedTime timer = new ElapsedTime();
+    private Servo light;
     private double lastError = 0;
 
-    private static final double flywheelOffset = 0;
-    private static final double flywheelMinSpeed = 1040;
-    private static final double flywheelMaxSpeed = 1300;
+    private static final double flywheelMinSpeed = 940;
+    private static final double flywheelMaxSpeed = 1250;
     private static final double scoreHeight = 26;
     private static final double passthroughPointRadius = 2;
 
     public double flywheelInchesPerSec;
 
-    PIDFCoefficients shooterLPIDF = new PIDFCoefficients(60.0, 0.0, 0.0, 11.7541);
-    PIDFCoefficients shooterRPIDF = new PIDFCoefficients(60.0, 0.0, 0.0, 11.775);
+    PIDFCoefficients shooterLPIDF = new PIDFCoefficients(120.0, 0.0, 0.0, 13.1);
+    PIDFCoefficients shooterRPIDF = new PIDFCoefficients(120.0, 0.0, 0.0, 12.5);
+    PIDFCoefficients beltPIDF = new PIDFCoefficients(0.0, 0.0, 0.0, 12.7);
     PIDFCoefficients aimPIDF = new PIDFCoefficients(1.2, 0.0, 0.1, 0.02); //may need to increase for shooting on the move
 
     public robotControl(HardwareMap hardwareMap, Follower follower) {
@@ -51,44 +62,20 @@ public class robotControl {
         ShooterR.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, shooterRPIDF);
         intake = hardwareMap.get(DcMotor.class, "intake");
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        belt = hardwareMap.get(DcMotor.class, "belt");
+        belt = hardwareMap.get(DcMotorEx.class, "belt");
         belt.setDirection(DcMotor.Direction.REVERSE);
+        belt.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        belt.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, beltPIDF);
         BlueBoi = hardwareMap.get(Servo.class, "BlueBoi");
         BlueBoi.setPosition(0.65);
         kickstand1 = hardwareMap.get(Servo.class, "kickstand1");
         kickstand2 = hardwareMap.get(Servo.class, "kickstand2");
+        light = hardwareMap.get(Servo.class, "light");
 
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
         limelight.start();
         timer.reset();
-    }
-
-    public double autoAim() {
-        Pose currentPose = follower.getPose();
-        double distanceX = goalTarget.getX() - currentPose.getX();
-        double distanceY = goalTarget.getY() - currentPose.getY();
-        double angleToGoal = Math.atan2(distanceY, distanceX) + Math.PI;
-
-        double error = angleToGoal - currentPose.getHeading();
-        while (error > Math.PI) error -= 2 * Math.PI;
-        while (error < -Math.PI) error += 2 * Math.PI;
-        double dt = timer.seconds();
-        timer.reset();
-
-        double derivative = 0;
-        if (dt > 0.001) {
-            derivative = (error - lastError) / dt;
-            lastError = error;
-        }
-        double feedForward = Math.signum(error) * aimPIDF.f;
-        double aimPower = (error * aimPIDF.p) + (derivative * aimPIDF.d) + feedForward;
-
-        if (Math.abs(error) < Math.toRadians(1.0))
-            return 0;
-        else {
-            return Range.clip(aimPower, -1.0, 1.0);
-        }
     }
 
     public ShotParameters updateShooting() {
@@ -112,7 +99,7 @@ public class robotControl {
         if (Math.abs(error) < Math.toRadians(1.0))
             return new ShotParameters(shotParameters.flywheelTicks, 0);
         else {
-            return new ShotParameters(shotParameters.flywheelTicks, Range.clip(aimPower, -1.0, 1.0));
+            return new ShotParameters(shotParameters.flywheelTicks, Range.clip(aimPower, -0.8, 0.8));
         }
     }
 
@@ -145,26 +132,76 @@ public class robotControl {
         kickstand2.setPosition(position);
     }
 
+    public void setLightColor(double value) {
+        light.setPosition(value);
+    }
+
     public void kickstandUp() {
-        kickstand1.setPosition(0.48);
-        kickstand2.setPosition(0.65);
+        kickstand1.setPosition(0.14);
+        kickstand2.setPosition(0.88);
     }
 
     public void kickstandDown() {
-        kickstand1.setPosition(0.96);
-        kickstand2.setPosition(0.15);
+        kickstand1.setPosition(0.55);
+        kickstand2.setPosition(0.48);
     }
 
-    //kickstand up 1: 0.48 2: 0.65
-    //kickstand down 1: 0.96  2: 0.15
+    public boolean relocalize() {
+        limelight.updateRobotOrientation(Math.toDegrees(follower.getHeading()));
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            Pose3D limelightPose = result.getBotpose_MT2();
 
-    public void setAlliance(String goalColor) {
-        switch (goalColor) {
-            case "blue":
+            //Limelight meters to pedro inches
+            double xInches = limelightPose.getPosition().x * 39.3701;
+            double yInches = limelightPose.getPosition().y * 39.3701;
+
+            //Limelight degrees to pedro radians
+            double yawRadians = Math.toRadians(limelightPose.getOrientation().getYaw());
+
+            Pose pedroPose = new Pose(xInches, yInches, yawRadians, FTCCoordinates.INSTANCE)
+                    .getAsCoordinateSystem(PedroCoordinates.INSTANCE);
+            follower.setPose(pedroPose);
+            return true;
+        }
+        else
+            return false;
+    }
+
+//    public void setAlliance(String goalColor) {
+//        switch (goalColor) {
+//            case "blue":
+//                goalTarget = new Pose(4, 140);
+//                endgamePark = () -> follower.pathBuilder()
+//                        .addPath(new Path(new BezierLine(follower::getPose, new Pose(112, 29))))
+//                        .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(135), 0.6))
+//                        .build();
+//                break;
+//            case "red":
+//                goalTarget = new Pose(140, 140);
+//                endgamePark = () -> follower.pathBuilder()
+//                        .addPath(new Path(new BezierLine(follower::getPose, new Pose(32, 29))))
+//                        .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.6))
+//                        .build();
+//                break;
+//        }
+//    }
+
+    public void setAlliance(Alliance alliance) {
+        switch (alliance) {
+            case BLUE:
                 goalTarget = new Pose(4, 140);
+                endgamePark = () -> follower.pathBuilder()
+                        .addPath(new Path(new BezierLine(follower::getPose, new Pose(112, 29))))
+                        .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(135), 0.6))
+                        .build();
                 break;
-            case "red":
+            case RED:
                 goalTarget = new Pose(140, 140);
+                endgamePark = () -> follower.pathBuilder()
+                        .addPath(new Path(new BezierLine(follower::getPose, new Pose(32, 29))))
+                        .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.6))
+                        .build();
                 break;
         }
     }
@@ -176,8 +213,7 @@ public class robotControl {
     }
 
     public static double getFlywheelTicksFromVelocity(double velocity) {
-        //return Range.clip((0.0176676 * Math.pow(velocity, 2)) - (5.11447 * velocity) + 1366.42995 + flywheelOffset, flywheelMinSpeed, flywheelMaxSpeed);
-        return Range.clip(3.33709 * velocity + 387.24308, flywheelMinSpeed, flywheelMaxSpeed);
+        return Range.clip((0.024369 * velocity * velocity) - (8.21129 * velocity) + 1632.4453, flywheelMinSpeed, flywheelMaxSpeed);
     }
 
     public ShotParameters calculateShotVectorAndTurret(Pose currentPose) {
@@ -211,10 +247,11 @@ public class robotControl {
         return new ShotParameters(flywheelTicks, heading);
     }
 
-    public void beltOn() { belt.setPower(0.8); }
+    public void beltOnShoot() { belt.setVelocity(1600); }
+    public void beltOnIntake() { belt.setVelocity(2600); }
     public void beltOff() { belt.setPower(0.0); }
     public void intakeOn() { intake.setPower(1.0); }
-    public void intakeOff() { intake.setPower(0.0); }
+    public void intakeOff() { intake.setPower(0.05); }
     public void blueBoiOpen() { BlueBoi.setPosition(1.0); }
     public void blueBoiClosed() { BlueBoi.setPosition(0.65); }
 }
